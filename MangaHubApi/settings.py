f303bea248f3d7e6ed99bb7d2fecf267cbe12961
ninjadev23 <ninja48140@gmail.com>
2025-08-31 16:42:46 -0400

    config for railway

--- MangaHubApi/settings.py
+++ MangaHubApi/settings.py
@@ -131,8 +131,7 @@ STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+MEDIA_ROOT = (BASE_DIR.parent / "media")
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
